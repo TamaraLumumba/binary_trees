c799bc653657e82023365462f046d53538a8f6b8
Rigiih7 <mwirigihbrian@gmail.com>
2023-11-02 16:39:13 +0300

    binary tree is full error reset

--- 16-binary_tree_is_perfect.c
+++ 16-binary_tree_is_perfect.c
@@ -35,21 +35,12 @@ void this_tree(const binary_tree_t *tree, size_t n,
  */
 int binary_tree_is_perfect(const binary_tree_t *tree)
 {
-	int result = 0;
+	size_t leaves_count = 0;
+	size_t tree_height = 0;
 
-	if (tree == NULL)
-	{
-		return (0);
-	}
-	else
-	{
-		result = tree_is_perfect(tree);
-		if (result != 0)
-		{
-			return (1);
-		}
-		return (0);
-	}
+	this_tree(tree, 0, &leaves_count, &tree_height);
+	return ((int)leaves_count == (1 << tree_height) ? 1 : 0);
 }
+
 
 
